f301c70b9ca57b832e7bd0ddc18c6f24e4ac165a
Jhonny07 <jhonnyjacinto09@icloud.com>
2022-05-24 19:23:08 +0000

    adicionando arquivos

---
+++ src/main/java/application/repositories/PlataformaRepository.java
@@ -0,0 +1,9 @@
+package application.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import application.models.Genero;
+
+public interface FeneroRepository extends CrudRepository<Plataforma, Integer>{
+    
+}
