388a010f38aede16d19242db959c095f85301c83
Jhonny07 <jhonnyjacinto09@icloud.com>
2022-05-24 19:23:19 +0000

    adicionando arquivos

--- src/main/java/application/models/Jogo.java
+++ src/main/java/application/models/Jogo.java
@@ -1,53 +1,60 @@
 package application.models;
-
+ 
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn
-import javax.persistence.JoinTable
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-
+ 
 import java.util.Set;
 import java.util.HashSet;
 @Entity
-@Table(name="generos")
+@Table(name="jogos")
 public class Jogo {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
     private String titulo;
-
+ 
     @ManyToMany(mappedBy = "id_genero")
     private Genero genero;
-
+ 
     @ManyToMany
     @JoinTable(
         name = "jogos_possuem_plataformas",
-        joinColumns = @joinColumn(name = "jogos_id"),
-        inverseJoinColumns=@joinColumn(name="plataformas_id"))
-
-    private Set<Jogo> jogos = new HashSet<>();
-
+        joinColumns = @JoinColumn(name = "jogos_id"),
+        inverseJoinColumns=@JoinColumn(name="plataformas_id"))
+    
+    private Set<Plataforma> plataformas = new HashSet<>();
+ 
     public void setId(int id) {
         this.id = id;
     }
     public int getId() {
         return this.id;
     }
+ 
+   
     public String getTitulo() {
         return titulo;
     }
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-    
-    public Set<Jogo> getJogos() {
-        return jogos;
+    public Genero getGenero() {
+        return genero;
     }
-    public void setJogos(Set<Jogo> jogos) {
-        this.jogos = jogos;
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+    pulic Set<Plataforma> getPlataformas() {
+        retur plataformas;
+    }
+    public void setPlaformas(Set<Plataforma> plataformas) {
+        this.plataformas = plataformas;
     }
     
 }
